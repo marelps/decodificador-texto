b4c25515041b8295b1730aab7beef90bc8f94181
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-08-19 13:07:25 -0300

    feat: :art: Implementação de novos estilos do projeto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("meuLink").addEventListener("click", function(event){
+    if(this.hasAttribute("disabled")){
+      event.preventDefault(); // Evita a ação padrão do link
+    }
+  });
+
+
+
+  const TextArea = document.querySelector("")
